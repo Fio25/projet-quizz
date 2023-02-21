a316418cf5b0cc3f153b9e5c52140f1b5bf84dd0
Fio25 <119542407+Fio25@users.noreply.github.com>
2023-02-21 14:41:29 +0100

    questions random

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Categories.css";
 import { Link } from "react-router-dom";
 
 function Categories() {
-
-  const questions = "http://127.0.0.1:8000/api/questions";
-  
-  const [questionsData, setQuestionsData] = useState({})
-  
-  // const [quizzData, setQuizzData] = useState({id:1, id: 2, id:3, id:4, id:5})
-
-  useEffect(() => {
-    fetch(questions)
-    .then((response) => response.json())
-    .then((data) => {
-        setQuestionsData(data)
-        // setQuizzData(data)
-        setIsLoading(false);
-    });
-  });
-
-  //Page de chargement le temps d'attendre l'API
-  const [isLoading, setIsLoading] = useState(true);
-
-  if (isLoading) {
-    return (
-      <div>
-        <div id="generated">Loading...</div>
-      </div>
-    );
-  }
-
   return (
     <>
       <div className="icons">
@@ -43,21 +15,13 @@ function Categories() {
         </Link>
       </div>
 
-      <div>
-        {questionsData.map(question => (
-          <p key={question.id}>{question.question}</p>
-        ))}
-      </div>
+
 
       <div className="blockCategories">
-
-          {/* <Link to="/Quizz/1" id={"Science"}>Science</Link>
-          <Link to="/Quizz/1" id={"Vélo"}>SCience</Link> */}
-
-        <Link className="btn-categories">Sciences</Link>
-        <Link className="btn-categories">Français</Link>
-        <Link className="btn-categories">Géographie</Link>
-        <Link className="btn-categories">Sport</Link>
+        <Link className="btn-categories" to="/Quizz/1">Sciences</Link>
+        <Link className="btn-categories" to="/Quizz/2">Français</Link>
+        <Link className="btn-categories" to="/Quizz/3">Géographie</Link>
+        <Link className="btn-categories" to="/Quizz/4">Sport</Link> 
       </div>
 
     </>
---
+++ src/Components/Reponse.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
+
+export default function Reponse({datas}) {
+  const [sortedResponse, setSortedResponse] = useState([])
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+
+    useEffect(() => {
+        const rdmresult = [
+          datas[currentQuestion].reponse2,
+          datas[currentQuestion].reponse3,
+          datas[currentQuestion].reponse4,
+          datas[currentQuestion].reponse5,
+          datas[currentQuestion].reponse6,
+          datas[currentQuestion].reponse7,
+          datas[currentQuestion].reponse8,
+          datas[currentQuestion].reponse9,
+          datas[currentQuestion].reponse10,
+        ];
+        const newSortResult = rdmresult
+        .sort(() => Math.random() - 0.5)
+        .slice(0,3)
+        .concat(datas[currentQuestion].reponse1)
+        .sort(() => Math.random() - 0.5)
+        setSortedResponse(newSortResult)
+    }, [currentQuestion, datas])
+
+    function wrongAnswer() {
+      setCurrentQuestion(currentQuestion+1)
+      console.log("t'es nul");
+    }
+
+    function goodAnswer(){
+      setCurrentQuestion(currentQuestion+1)
+      console.log("t'es trop fort");
+    }
+
+    function returnBasic() {
+      return (
+        <div>
+            <p>{datas[currentQuestion].question}</p>
+            {sortedResponse.map((row) => (
+            <button key={uuidv4()} onClick={row === datas[currentQuestion].reponse1 ? goodAnswer : wrongAnswer}>{row}</button>
+          ))}
+        </div>
+      )
+    }
+
+    
+    function returnEnd() {
+      return (
+        <div>
+          <p>gg t'as fini</p>
+        </div>
+      )
+    }
+  return currentQuestion < 10 ? returnBasic() : returnEnd()
+}
