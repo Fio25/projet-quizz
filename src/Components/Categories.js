c89d13c03733d4a259d9608ffd08977a8101a04b
Fio25 <119542407+Fio25@users.noreply.github.com>
2023-02-22 09:44:52 +0100

    timer et score

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -18,9 +18,9 @@ function Categories() {
 
 
       <div className="blockCategories">
-        <Link className="btn-categories" to="/Quizz/1">Sciences</Link>
+        <Link className="btn-categories" to="/Quizz/1">Géographie</Link>
         <Link className="btn-categories" to="/Quizz/2">Français</Link>
-        <Link className="btn-categories" to="/Quizz/3">Géographie</Link>
+        <Link className="btn-categories" to="/Quizz/3">Sciences</Link>
         <Link className="btn-categories" to="/Quizz/4">Sport</Link> 
       </div>
 
--- src/Components/Reponse.js
+++ src/Components/Reponse.js
@@ -1,60 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import './Reponse.css'
-import {v4 as uuidv4} from 'uuid'
+import React, { useEffect, useState } from "react";
+import "./Reponse.css";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Reponse({datas}) {
-  const [sortedResponse, setSortedResponse] = useState([])
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+export default function Reponse({ datas }) {
+  const [sortedResponse, setSortedResponse] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
+  useEffect(() => {
+    const rdmresult = [
+      datas[currentQuestion].reponse2,
+      datas[currentQuestion].reponse3,
+      datas[currentQuestion].reponse4,
+      datas[currentQuestion].reponse5,
+      datas[currentQuestion].reponse6,
+      datas[currentQuestion].reponse7,
+      datas[currentQuestion].reponse8,
+      datas[currentQuestion].reponse9,
+      datas[currentQuestion].reponse10,
+    ];
+    const newSortResult = rdmresult
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3)
+      .concat(datas[currentQuestion].reponse1)
+      .sort(() => Math.random() - 0.5);
+    setSortedResponse(newSortResult);
+    setTimer(20);
+  }, [currentQuestion, datas]);
 
-    useEffect(() => {
-        const rdmresult = [
-          datas[currentQuestion].reponse2,
-          datas[currentQuestion].reponse3,
-          datas[currentQuestion].reponse4,
-          datas[currentQuestion].reponse5,
-          datas[currentQuestion].reponse6,
-          datas[currentQuestion].reponse7,
-          datas[currentQuestion].reponse8,
-          datas[currentQuestion].reponse9,
-          datas[currentQuestion].reponse10,
-        ];
-        const newSortResult = rdmresult
-        .sort(() => Math.random() - 0.5)
-        .slice(0,3)
-        .concat(datas[currentQuestion].reponse1)
-        .sort(() => Math.random() - 0.5)
-        setSortedResponse(newSortResult)
-    }, [currentQuestion, datas])
-
-    function wrongAnswer() {
-      setCurrentQuestion(currentQuestion+1)
-      console.log("t'es nul");
+  //TIMER
+  const [timer, setTimer] = useState(20);
+  const [timerId, setTimerId] = useState(null);
+  useEffect(() => {
+    if (timer > 0) {
+      setTimerId(setTimeout(() => setTimer((prev) => prev - 1), 1000));
     }
-
-    function goodAnswer(){
-      setCurrentQuestion(currentQuestion+1)
-      console.log("t'es trop fort");
+    if (timer === 0) {
+      clearTimeout(timerId);
     }
+    return () => clearInterval(timerId);
+  }, [timer]);
 
-    function returnBasic() {
-      return (
-        <div>
-            <p>{datas[currentQuestion].question}</p>
-            {sortedResponse.map((row) => (
-            <button key={uuidv4()} onClick={row === datas[currentQuestion].reponse1 ? goodAnswer : wrongAnswer}>{row}</button>
+  function wrongAnswer() {
+    setCurrentQuestion(currentQuestion + 1);
+    console.log("t'es nul");
+  }
+
+  function goodAnswer() {
+    setScore(score + 1);
+    setCurrentQuestion(currentQuestion + 1);
+    console.log("t'es trop fort");
+  }
+
+  function returnBasic() {
+    return (
+      <div>
+
+        <p className="timer"><i className="fa-regular fa-clock"></i> {timer} secondes</p>
+
+        <div className="question-block">
+
+          <div className="currentQuest">
+            <p className="currentQuestionText">{datas[currentQuestion].question}</p>
+          </div>
+
+          {sortedResponse.map((row) => (
+            <button className="btns-question" key={uuidv4()} onClick={row === datas[currentQuestion].reponse1 ? goodAnswer : wrongAnswer}>
+              {row}
+            </button>
           ))}
-        </div>
-      )
-    }
 
-
-    function returnEnd() {
-      return (
-        <div>
-          <p>gg t'as fini</p>
         </div>
-      )
-    }
-  return currentQuestion < 10 ? returnBasic() : returnEnd()
+        
+      </div>
+    );
+  }
+
+  function returnEnd() {
+    return (
+      
+        <p className="score">Score : {score}/10</p>
+      
+    );
+  }
+
+  return currentQuestion < 10 ? returnBasic() : returnEnd();
 }
