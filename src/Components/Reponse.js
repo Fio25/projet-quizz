f71166a649a08795b9e6c7912ecf044712bfa66f
Julian Brasseur <brasseurjulian2@gmail.com>
2023-02-23 11:31:30 +0100

    Modifications CSS questions et score final

--- src/Components/Reponse.js
+++ src/Components/Reponse.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./Reponse.css";
 import { v4 as uuidv4 } from "uuid";
+import { Link } from "react-router-dom";
 
 export default function Reponse({ datas }) {
-
   const [sortedResponse, setSortedResponse] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [datasQuestion, setDatasQuestion] = useState(datas)
 
   useEffect(() => {
     const rdmresult = [
-      datas[currentQuestion].reponse2,
-      datas[currentQuestion].reponse3,
-      datas[currentQuestion].reponse4,
-      datas[currentQuestion].reponse5,
-      datas[currentQuestion].reponse6,
-      datas[currentQuestion].reponse7,
-      datas[currentQuestion].reponse8,
-      datas[currentQuestion].reponse9,
-      datas[currentQuestion].reponse10,
+      datasQuestion[currentQuestion].reponse2,
+      datasQuestion[currentQuestion].reponse3,
+      datasQuestion[currentQuestion].reponse4,
+      datasQuestion[currentQuestion].reponse5,
+      datasQuestion[currentQuestion].reponse6,
+      datasQuestion[currentQuestion].reponse7,
+      datasQuestion[currentQuestion].reponse8,
+      datasQuestion[currentQuestion].reponse9,
+      datasQuestion[currentQuestion].reponse10,
     ];
     const newSortResult = rdmresult
       .sort(() => Math.random() - 0.5)
@@ -27,7 +28,7 @@ export default function Reponse({ datas }) {
       .sort(() => Math.random() - 0.5);
     setSortedResponse(newSortResult);
     setTimer(20);
-  }, [currentQuestion, datas]);
+  }, [currentQuestion, datasQuestion]);
 
   //TIMER
   const [timer, setTimer] = useState(20);
@@ -35,59 +36,98 @@ export default function Reponse({ datas }) {
   const [stopTimer, setStopTimer] = useState(false);
 
   useEffect(() => {
-    if (!stopTimer) {    
-    if (timer > 0) {
-      setTimerId(setTimeout(() => setTimer((prev) => prev - 1), 1000));
-    }
-    if (timer === 0) {
-      clearTimeout(timerId);
-      wrongAnswer()
-    }
+    if (!stopTimer) {
+      if (timer > 0) {
+        setTimerId(setTimeout(() => setTimer((prev) => prev - 1), 1000));
+      }
+      if (timer === 0) {
+        clearTimeout(timerId);
+        wrongAnswer();
+      }
 
-    return () => clearInterval(timerId);
-  } }, [timer]);
+      return () => clearInterval(timerId);
+    }
+  }, [timer]);
 
   //MAUVAISE REPONSE
   function wrongAnswer() {
     setCurrentQuestion(currentQuestion + 1);
-    console.log("t'es nul");
   }
 
   //BONNE REPONSE
   function goodAnswer() {
     setScore(score + 1);
     setCurrentQuestion(currentQuestion + 1);
-    console.log("t'es trop fort");
+  }
+
+  //RESTART
+  function restart() {
+    setDatasQuestion(datasQuestion.sort(() => Math.random() - 0.5))
+    setCurrentQuestion(sortedResponse.sort(() => Math.random() - 0.5));
+    setTimer(20)
+    setScore(0)
+    setCurrentQuestion(0)
+    setStopTimer(false)
+    console.log(sortedResponse);
   }
 
   function returnBasic() {
     return (
       <div className="question-loca">
-
-        <p className="timer"><i className="fa-regular fa-clock"></i> {timer} secondes</p>
-
-        <div className="currentQuest">
-          <p className="currentQuestionText">{datas[currentQuestion].question}</p>
-        </div>
+        <p className="timer">
+          <i className="fa-regular fa-clock"></i> {timer} secondes
+        </p>
+        <div className="questions">
+          <div className="currentQuest">
+            <p className="currentQuestionText">
+              {datasQuestion[currentQuestion].question}
+            </p>
+          </div>
 
           {sortedResponse.map((row) => (
-            <button className="btns-question" key={uuidv4()} onClick={row === datas[currentQuestion].reponse1 ? goodAnswer : wrongAnswer}>
+            <button
+              className="btns-question"
+              key={uuidv4()}
+              onClick={
+                row === datasQuestion[currentQuestion].reponse1
+                  ? goodAnswer
+                  : wrongAnswer
+              }
+            >
               {row}
             </button>
           ))}
-
+        </div>
       </div>
     );
   }
 
   function returnEnd() {
+    if (!stopTimer) setStopTimer(true);
 
-    if (!stopTimer) setStopTimer(true)
-    
     return (
-      
-        <p className="scoreFinal">Score : {score}/10</p>
-      
+      <div>
+        <div className="end-animation">
+        {score >= 5 ? 
+    <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ftxa7lsy.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>  
+    : 
+    <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_ccxfskpm.json"  background="transparent"  speed="1" loop  autoplay></lottie-player>
+  }
+
+        </div>
+        {score >= 5 ? 
+      <p className="scoreFinal cassou-shiba">Cassou le shiba est content tu as eu plus de 5 !</p>
+    : 
+    <p className="scoreFinal cassou-shiba">Cassou le shiba est très triste tu as eu moins de 5 !</p>
+    
+  }
+     <p className="scoreFinal">Score : {score}/10</p>
+      <div className="container-btn">
+
+     <button onClick={restart} className="btns-question btn-replay">Rejouer</button>
+
+      </div>
+     </div>
     );
   }
 
